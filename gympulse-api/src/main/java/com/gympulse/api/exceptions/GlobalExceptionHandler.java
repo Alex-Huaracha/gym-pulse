381f5ea10aedab8c42d0a95efc661b7d1c3d663c
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-12-21 16:01:59 -0500

    chore: setup basic security and global exception handling

---
+++ gympulse-api/src/main/java/com/gympulse/api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.gympulse.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    public ProblemDetail handleRuntimeException(RuntimeException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Resource Not Found");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+}
