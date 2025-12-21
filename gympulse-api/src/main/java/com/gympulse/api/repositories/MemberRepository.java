36a5ee4f5ace2f8a03eb6d6dcf5495410669a9c2
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-12-21 16:02:56 -0500

    feat(member): add repository with custom query methods

---
+++ gympulse-api/src/main/java/com/gympulse/api/repositories/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.gympulse.api.repositories;
+
+import com.gympulse.api.models.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    // Custom query method to find a member by DNI
+    Optional<Member> findByDni(String dni);
+
+    // Custom query method to count members by status
+    long countByStatus(String status);
+
+    // Custom query method for searching members by lastname or firstname containing
+    List<Member> findByLastNameContainingIgnoreCaseOrFirstNameContainingIgnoreCase(String lastName, String firstName);
+
+}
