d42d8d11a68ca2f0dc5c091bfea94283b060214e
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-12-25 19:52:30 -0500

    feat(membership): add MembershipRepository with methods for membership queries

---
+++ gympulse-api/src/main/java/com/gympulse/api/repositories/MembershipRepository.java
@@ -0,0 +1,15 @@
+package com.gympulse.api.repositories;
+
+import com.gympulse.api.models.Membership;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MembershipRepository extends JpaRepository<Membership, Integer> {
+    List<Membership> findByMemberId(Integer memberId);
+    // Method to find an active membership by user ID
+    Optional<Membership> findByMemberIdAndIsPaidTrue(Integer memberId);
+}
